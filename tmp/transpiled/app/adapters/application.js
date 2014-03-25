b72af03540cc335367c1153ae63367e35c7bac16
elisam98 <cornbeefonrye@hotmail.com>
2014-03-25 14:02:08 -0400

    added package file

--- tmp/transpiled/app/adapters/application.js
+++
@@ -1,6 +0,0 @@
-define("appkit/adapters/application", 
-  ["exports"],
-  function(__exports__) {
-    "use strict";
-    __exports__["default"] = DS.FixtureAdapter.extend();
-  });
--- tmp/transpiled/app/utils/ajax.js
+++
@@ -1,9 +0,0 @@
-define("appkit/utils/ajax", 
-  ["exports"],
-  function(__exports__) {
-    "use strict";
-    /* global ic */
-    __exports__["default"] = function ajax(){
-      return ic.ajax.apply(null, arguments);
-    }
-  });
